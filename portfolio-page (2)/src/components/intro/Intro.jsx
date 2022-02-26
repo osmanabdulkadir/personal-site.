6c53452f2294b65a6dff6dd17da8cc883403a46f
osman abdul <74805866+osmanabdulkadir@users.noreply.github.com>
2022-02-26 16:22:15 -0500

    Add files via upload

---
+++ portfolio-page (2)/src/components/intro/Intro.jsx
@@ -0,0 +1,37 @@
+import "./intro.scss";
+import { useEffect, useRef } from "react";
+import { init } from "ityped";
+
+export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      strings: ["Developer", "deginer", "Content Creator"],
+      backDelay: 2000,
+      backSpeed: 60,
+      showCursor: true
+    });
+  }, []);
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Osman Abdulkadir</h1>
+          <h3>
+            Freelance <span ref={textRef}> </span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="osman abdul" />
+        </a>
+      </div>
+    </div>
+  );
+}
